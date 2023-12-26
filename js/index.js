9130f7a3fea966d10711438c66d23f737e8a422d
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2023-12-25 23:14:23 -0500

    Setup a feed system
    Created a rudimentary feed system in which I can store projects in a JSON file and render pages with them. This will allow for easier setup of different categories, numbered feed pages, etc.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,25 @@
 function setFrame(frame,container,elementName){
     document.getElementById(container).innerHTML = frame.contentWindow.document.getElementById(elementName).innerHTML;
 }
+
+//function to import project data from json file using jquery
+$(document).ready(function(){
+    console.log('got this far 0');
+    $.getJSON("/json/projects.json", function(data){//import json data
+        console.log(data.projects.length);
+        for (var i = 0; i < data.projects.length; i++){ //iterate over the projects in the json
+            addProject(data.projects[i].id, data.projects[i].title, data.projects[i].categories, data.projects[i].bodytext, data.projects[i].imagetext); //for each project render and append
+        }
+    })
+})
+
+//function to append a div for a project with imported data
+function addProject(id, title, categories, bodytext, imagetext){
+    $('body').append(`
+    <div class="maintext" id="${id}">
+        <h1><strong>${title}</h1>
+		${imagetext}
+        <p class="indented">${bodytext}</p>
+	</div>
+    `);
+}

ddb2ce4a3ae834b25188b0830b32125a236dc764
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2024-01-08 16:15:02 -0500

    Migration to JSON project feed
    Moved projects to a blog post-style renderer reading from a JSON file. Updated bio to reflect my graduation.

--- js/index.js
+++ js/index.js
@@ -4,15 +4,17 @@ function setFrame(frame,container,elementName){
 }
 
 //function to import project data from json file using jquery
-$(document).ready(function(){
+function renderPage(page_category = 'all'){
     console.log('got this far 0');
     $.getJSON("/json/projects.json", function(data){//import json data
         console.log(data.projects.length);
         for (var i = 0; i < data.projects.length; i++){ //iterate over the projects in the json
-            addProject(data.projects[i].id, data.projects[i].title, data.projects[i].categories, data.projects[i].bodytext, data.projects[i].imagetext); //for each project render and append
+            if(data.projects[i].categories.includes(page_category) || page_category == 'all'){
+                addProject(data.projects[i].id, data.projects[i].title, data.projects[i].categories, data.projects[i].bodytext, data.projects[i].imagetext); //for each project render and append
+            }
         }
     })
-})
+}
 
 //function to append a div for a project with imported data
 function addProject(id, title, categories, bodytext, imagetext){

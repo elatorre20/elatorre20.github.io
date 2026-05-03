386c337dac746337f29960f8ff8ebb3f23b9051e
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2026-05-03 01:50:53 -0400

    Pin CR10-S digital twin project ahead of existing pinned items

--- js/index.js
+++ js/index.js
@@ -60,6 +60,7 @@ function compareProjectsByDate(leftProject, rightProject){
 }
 
 const PINNED_PROJECT_ORDER = [
+    'cr10-s-3d-printer-digital-twin',
     'antikythera',
     'gear',
     'ysi-weather-station'
